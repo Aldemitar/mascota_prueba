2350f46846ab781de92312efcc84e982e31f379a
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-21 22:35:13 -0500

    Se cambio el main para el uso de Jinjar

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+
+import home
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
-
-templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "mensaje": "¡Hola desde FastAPI!"})
+app.include_router(home.router)
+
+templates = Jinja2Templates(directory="templates")
